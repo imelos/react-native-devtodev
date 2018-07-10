26fe5e0d2cc9abfaf320133e6592568022cab60d
pro <imelose@gmail.com>
2018-07-10 15:21:04 +0300

    yarn lock, and devtodev methods

--- index.js
+++ index.js
@@ -7,4 +7,10 @@ export default {
     init(appId: string, secretKey: string) {
         DevToDev.init(appId, secretKey);
     },
+    setCurrentLevel(currentLevel: number) {
+        DevToDev.setCurrentLevel(currentLevel);
+    },
+    setUserId(activeUserId: string) {
+        DevToDev.setUserId(activeUserId);
+    },
 };
